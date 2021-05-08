f7e3d36dc2951f34865ec488805923c71c62c005
Margarita-Z <81475993+Margarita-Z@users.noreply.github.com>
2021-05-08 02:05:33 +0200

    Add files via upload

---
+++ Domasna1/domasno1.js
@@ -0,0 +1,50 @@
+/* 1 */
+let homeWork = "one";
+let number = 01;
+let isEasy = true;
+let isDifficult = false;
+let unNume;
+let vrednost = null;
+/* 2 */
+console.log(homeWork);
+console.log(number);
+console.log(isEasy);
+console.log(isDifficult);
+console.log(unNume);
+console.log(vrednost);
+/* 3 */
+console.log(typeof homeWork);
+console.log(typeof number);
+console.log(typeof isEasy);
+console.log(typeof isDifficult);
+console.log(typeof unNume);
+console.log(typeof vrednost);
+/* 4 */
+const metarKvadraten = "m2";
+console.log(metarKvadraten);
+/* 5 */
+/*const metarKvadraten = "m3";
+console.log(metarKvadraten);*/   /* Javuva greska deka konstantata ne moze da bide predeklarirana so druga vrednost bidejki vrednosta za taa promenliva e konstatna*/
+/* 6 */
+let fruit1 = {
+    Name: "Cherry",
+    color: "red",
+    form: "circular"
+};
+let fruit2 = {
+    Name: "watermelon",
+    color: "green",
+    form: "circular"
+};
+let fruits = [fruit1, fruit2];
+/* 7 */
+console.log(fruit1);
+console.log(fruit2);
+console.log(fruits);
+/* 8 */
+console.log(typeof fruit1);
+console.log(typeof fruit2);
+console.log(typeof fruits);
+/*   Bonus 
+Tipot na promenlivata vo nizata e object
+*/
