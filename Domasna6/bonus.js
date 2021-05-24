219f7d895eea5c9c7d81492ebea9e38de2108d15
Margarita-Z <81475993+Margarita-Z@users.noreply.github.com>
2021-05-25 01:35:07 +0200

    Add files via upload

---
+++ Domasna6/bonus.js
@@ -0,0 +1,17 @@
+// 1.
+document.body.style.fontFamily = 'Arial, sans-serif';
+// 2.
+document.getElementById('nickname').textContent = 'Margarita-Z';
+document.getElementById('favorites').textContent = 'javascript';
+document.getElementById('hometown').textContent = 'Radovis';
+//3.
+let list = document.getElementsByTagName('li');
+for (let i = 0; i < items.length; i++) {
+   list[i].className = 'listItem';
+}
+let mouse_over = () => {
+    list.style.color = 'font';
+    list.style.backgroundColor = 'red';
+    list.style.font size = '14px';
+};
+list.addEventListener('mouseover', mouse_over);
